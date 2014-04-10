dce969412a0b08abf2ff6a411b37533e160d5ea4
KoryNunn <knunn187@gmail.com>
2014-04-10 14:39:22 +1000

    Fixed content not having renderedElement bug, changed to _type

--- list.js
+++ list.js
@@ -1,22 +1,20 @@
 var Gaffa = require('gaffa'),
     crel = require('crel'),
-    TemplaterProperty = require('gaffa/templaterProperty'),
-    viewType = "list",
-	cachedElement;
+    TemplaterProperty = require('gaffa/templaterProperty');
 
 function List(){
     this.views.list = new Gaffa.ViewContainer(this.views.list);
     this.views.empty = new Gaffa.ViewContainer(this.views.empty);
 }
 List = Gaffa.createSpec(List, Gaffa.ContainerView);
-List.prototype.type = viewType;
+List.prototype._type = 'list';
 
 List.prototype.render = function(){
-
     var renderedElement = crel(this.tagName || 'div');
 
+    this.views.content.element = renderedElement;
     this.views.list.element = renderedElement;
-	this.views.empty.element = renderedElement;
+    this.views.empty.element = renderedElement;
     this.renderedElement = renderedElement;
 
 };
